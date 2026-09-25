5672b28633118fa3289b1a6605f3f4a690589d5c
Christopher Alba <chris.alba.v2@gmail.com>
2026-09-25 17:38:48 -0600

    renderizado condicional en about

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog de Autos';
+export const SITE_DESCRIPTION = 'Reseñas y especificaciones de los autos deportivos más emocionantes para puristas';
